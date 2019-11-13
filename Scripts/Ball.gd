b8546772c9928d3faf12a79310aa82c0cf816526
inewhart <inewhart@iu.edu>
2019-11-12 23:41:09 -0500

    Push

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -4,7 +4,10 @@ export var maxspeed = 300
 
 signal lives
 signal score
-
+var startpos
+var trauma = 0
+var decayrate= 0
+var maxoffset = 1
 func _ready():
  contact_monitor = true
  set_max_contacts_reported(4)
@@ -18,9 +21,24 @@ func _physics_process(delta):
   if body.is_in_group("Tiles"):
    emit_signal("score",body.score)
    body.queue_free()
+   var scene = "res://Scenes/Level1.tscn"
+   if bodies.length() == 0 && scene == "res://Scenes/Level1.tscn":
+    get_tree().change_scene("res://Scenes/Level2.tscn")
   if body.get_name() == "Paddle":
    pass
   
  if position.y > get_viewport_rect().end.y:
   emit_signal("lives")
   queue_free()
+func addtrauma(amount):
+	trauma = min(trauma + amount, 1)
+func traumadecay(delta):
+	var change = decayrate + delta
+	trauma = max(trauma - change,0)
+func shake():
+	var shake = trauma + trauma
+	var x = maxoffset + shake + _get_neg_or_pos_scalar()
+	var y = maxoffset + shake + _get_neg_or_pos_scalar()
+	ColorRect.rect_position = startpos + Vector2(x,y)
+func _get_neg_or_pos_scalar():
+	return rand_range(-1,1)
